79797fca68f5c8e1bedc3603a30c066bfc972791
Nate <Nate@10.4.215.197>
2019-05-16 14:46:17 -0500

    only god can save weingart now

--- JavaApplication41/src/PokemonRunner.java
+++ JavaApplication41/src/PokemonRunner.java
@@ -19,6 +19,7 @@ public class PokemonRunner {
         String myName = sc.nextLine();
         System.out.println("What is your rival's name?");
         String enemyName = sc.nextLine();
+        
         System.out.println("Hello " + myName + "! Which starter pokemon would you like?");
         Trainer me = new Trainer(myName);
         Trainer rival = new Trainer(enemyName);
@@ -26,7 +27,7 @@ public class PokemonRunner {
         System.out.println("1 Squirtle");
         System.out.println("2 Bulbasaur");
         int starterChoice = sc.nextInt();
-        Pokemon Charmander1 = new Pokemon("Charmander", 39, 52, 43, 1);
+        Pokemon Charmander1 = new Pokemon("Charmander", 43, 52, 43, 1);
         Charmander1.learnMove(new Move("Ember", 40));
         Pokemon Squirtle1 = new Pokemon("Squirtle", 44, 48, 65, 1);
         Squirtle1.learnMove(new Move("Bubble", 40));
@@ -45,11 +46,48 @@ public class PokemonRunner {
                 me.addPokemon(Bulbasaur1);
                 rival.addPokemon(Charmander1);
         }
+        
         System.out.println("Your rival " + rival.getName() + " wants to have a battle with your new Pokemon!");
-        Battle battle1 = new Battle(me, rival);
-        battle1.run();
-        System.out.println("You go to heal your Pokemon at the Pokemon center");
-        me.heal();
-        rival.heal();
+        Battle rivalBattle = new Battle(me, rival);
+        boolean rivalBattleResult = rivalBattle.run();
+        if (!rivalBattleResult) {
+            System.out.println(rival.getName() + " gives you some rare candies to help level your Pokemon up.");
+            me.getPokemon().get(0).levelUp(600, true);
+        }
+        System.out.println("You go to heal your Pokemon at the Pokemon center.");
+        me.heal(true);
+        rival.heal(false);
+        
+        System.out.println("You enter into the forest and come across a Pikachu.");
+        Trainer wild1 = new Trainer("Pikachu");
+        Pokemon Pikachu1 = new Pokemon("Pikachu", 35, 55, 40, 6);
+        Pikachu1.learnMove(new Move("Quick Attack", 40));
+        Pikachu1.learnMove(new Move("Thundershock", 50));
+        wild1.addPokemon(Pikachu1);
+        Battle pikachuBattle = new Battle(me, wild1);
+        boolean pikachuBattleResult = pikachuBattle.run();
+        if (pikachuBattleResult) {
+            System.out.println("You captured the Pikachu!");
+        } else {
+            System.out.println("The Pikachu took pity on you and joined your team.");
+            System.out.println("Also " + me.getPokemon().get(0).getName() + " gained some experience.");
+            me.getPokemon().get(0).levelUp(1300, true);
+        }
+        me.addPokemon(Pikachu1);
+        System.out.println(me.getPokemon().get(0).getName() + " learned Slam.");
+        me.getPokemon().get(0).learnMove(new Move("Slam", 80));
+        System.out.println("You go to heal your Pokemon at the Pokemon center.");
+        me.heal(true);
+        wild1.heal(false);
+        
+        System.out.println("Upon coming out Gary wants to fight again. He says that he's done a lot of training");
+        rival.getPokemon().get(0).learnMove(new Move("Tackle", 60));
+        rival.getPokemon().get(0).levelUp(1800 , false);
+        Pokemon Rattata1 = new Pokemon("Rattata", 30, 56, 35, 6);
+        Rattata1.learnMove(new Move("Scratch", 45));
+        rival.addPokemon(Rattata1);
+        rivalBattle.run();
+        
+        
     }
 }
